e3e6554f6a060df5efa774149457ef169b51d88d
hwangjeongwoo <hwangjeongwoo@gmail.com>
2022-05-31 12:02:17 +0900

    Add code for serving example

---
+++ serving_example/boston_housing.py
@@ -0,0 +1,27 @@
+from tensorflow import keras
+import numpy
+import json
+import os, sys, re
+
+def handler(data):
+
+    model_name = 'boston-housing'
+    os.chdir('/home/splunk/project/model')
+    current_path = os.getcwd()
+
+    event_body = data
+    print(event_body)
+
+    rows = 1
+    values_list = [[float(v) for v in event_body.values()] for i in range(rows)]
+
+    print(values_list)
+    np_arr_X = numpy.array(values_list)
+
+    reconstructed_model = keras.models.load_model(os.path.join(current_path, model_name))
+    ret = reconstructed_model.predict(np_arr_X).flatten()
+
+    return {
+        'statusCode': 200,
+        'body': ret.tolist()[0]
+    }
